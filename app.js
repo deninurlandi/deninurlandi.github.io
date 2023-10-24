a68105502c424816f5fba49c2fa6f0f5f0203a1e
deninurlandi <badjolex14@gmail.com>
2023-10-24 08:32:15 +0700

    modifikasi web

--- app.js
+++ app.js
@@ -2,11 +2,17 @@
 window.onscroll = function(){
    const header = document.querySelector('header');
    const fixedNav = header.offsetTop;
+   const toTop = document.querySelector('#to-top')
 
    if(window.scrollY > fixedNav){
       header.classList.add('navbar-fixed')
+      toTop.classList.remove('hidden')
+      toTop.classList.add('flex')
+
    }else{
       header.classList.remove('navbar-fixed')
+      toTop.classList.remove('flex')
+      toTop.classList.add('hidden')
    }
 }
 
@@ -18,4 +24,12 @@ const navMenu = document.querySelector('#nav-menu')
 hamberger.addEventListener('click', function(){
    hamberger.classList.toggle('hamberger-active');
    navMenu.classList.toggle('hidden')
+})
+
+// clik diluar hamberger
+window.addEventListener('click', function(e){
+   if(e.target != hamberger && e.target != navMenu){
+      hamberger.classList.remove('hamberger-active');
+      navMenu.classList.add('hidden')
+   }
 })
