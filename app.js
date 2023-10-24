6c95d0b3c1aa50d06014743abed9e1e8734704ff
deninurlandi <badjolex14@gmail.com>
2023-10-24 21:51:12 +0700

    menambahkan fitur dark mode

--- app.js
+++ app.js
@@ -33,3 +33,42 @@ window.addEventListener('click', function(e){
       navMenu.classList.add('hidden')
    }
 })
+
+// dark mode toggle
+const darkToggle = document.querySelectorAll('.dark-toggle');
+const html = document.querySelector('html');
+
+const toggles = document.querySelector('.toggle');
+const toggleS = document.querySelector('.toggle-1')
+
+darkToggle.forEach((e)=>{
+   e.addEventListener('click', function(){
+      if(e.checked){
+         html.classList.add('dark');
+         localStorage.theme = 'dark'
+         toggles.classList.add('toggle-circle')
+         toggleS.classList.add('toggle-circle')
+      }
+      else{
+         html.classList.remove('dark');
+         localStorage.theme = 'light'
+         toggles.classList.remove('toggle-circle')
+         toggleS.classList.remove('toggle-circle')
+      }
+      
+   })
+
+   if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      e.checked = true;
+      toggles.classList.add('toggle-circle')
+      toggleS.classList.add('toggle-circle')
+    } else {
+      e.checked = false;
+      toggles.classList.remove('toggle-circle')
+      toggleS.classList.remove('toggle-circle')
+    }
+})
+
+
+
+// pindahkan posisi 
